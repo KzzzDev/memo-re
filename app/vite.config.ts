f2c3db3759a1762284ef12cdc9f757cda4ab8bc3
KleinChiu <65886108+KleinChiu@users.noreply.github.com>
2022-10-26 07:48:28 +0000

    chore(project): :wrench: Configure project
    - use vite as dev server and bundler
    - code with typescript
    - format with prettier
    - add vscode task for starting dev server
    - add vscode extension recommendation

---
+++ app/vite.config.ts
@@ -0,0 +1,20 @@
+import vue from "@vitejs/plugin-vue";
+import { defineConfig } from "vite";
+import env from "vite-plugin-env-compatible";
+
+export default defineConfig({
+  plugins: [env({ prefix: "", mountedPath: "process.env" }), vue()],
+  base: "/",
+  build: {
+    cssCodeSplit: false,
+    rollupOptions: {
+      external: ["vue"],
+      output: {
+        format: "iife",
+        globals: {
+          vue: "Vue",
+        },
+      },
+    },
+  },
+});
