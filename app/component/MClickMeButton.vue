97f42aa7304b1bc4995c9ab7d670ba58628b5fdb
KleinChiu <65886108+KleinChiu@users.noreply.github.com>
2022-10-26 08:26:03 +0000

    feat: :sparkles: Bootstrap the project

---
+++ app/component/MClickMeButton.vue
@@ -0,0 +1,17 @@
+<template>
+  <button type="button" @click="click++">Click me</button>
+  <p>Click {{ click }} times</p>
+</template>
+
+<script setup lang="ts">
+import { ref } from "vue";
+
+const prop = defineProps({
+  start: {
+    type: Number,
+    required: true,
+  },
+});
+
+const click = ref(prop.start);
+</script>
---
+++ app/view/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <header>
+    <h1>Hello World</h1>
+  </header>
+
+  <main>
+    <m-click-me :start="0" />
+  </main>
+</template>
+
+<script setup lang="ts">
+import MClickMe from "../component/MClickMeButton.vue";
+</script>
+
+<style scoped>
+h1 {
+  color: green;
+}
+</style>
