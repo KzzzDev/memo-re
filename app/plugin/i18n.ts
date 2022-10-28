e8557f0a5458f9063d3289569ec3d250bf2e1814
KleinChiu <65886108+KleinChiu@users.noreply.github.com>
2022-10-28 12:37:46 +0000

    chore: :truck: Move locale file
    - from `i18n` to `locales`
    - rename files from `main` to locale name

--- app/plugin/i18n.ts
+++ app/plugin/i18n.ts
@@ -1,10 +1,8 @@
 import { createI18n } from "vue-i18n";
 
-import en from "../public/i18n/en/main.json";
-import ja from "../public/i18n/ja/main.json";
+import ja from "../public/locales/ja.json";
 
 export default createI18n({
   locale: "ja",
-  fallbackFormat: "en",
-  messages: { en, ja },
+  messages: { ja },
 });
