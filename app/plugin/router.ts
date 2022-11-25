e2b072369cbccd88cfb276d756b0df7737b203c4
Yuuma-ujimoto <yxy.yuuma@gmail.com>
2022-11-25 16:03:17 +0900

    space

--- app/plugin/router.ts
+++ app/plugin/router.ts
@@ -1,4 +1,4 @@
-import {createRouter, createWebHistory} from "vue-router";
+import { createRouter, createWebHistory } from "vue-router";
 
 export default createRouter({
     history: createWebHistory(),
