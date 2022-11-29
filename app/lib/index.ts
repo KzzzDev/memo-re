0df768cf2cf3b3a3ca6dd51979443e9b6ecd7e3b
KleinChiu <65886108+KleinChiu@users.noreply.github.com>
2022-11-29 18:50:44 +0000

    refactor: :recycle: Move plug in definition to lib

--- app/lib/index.ts
+++ app/lib/index.ts
@@ -1,11 +1,7 @@
+import { Component, App } from "vue";
+
 interface MemoReOptions {
   baseUrl: string;
-}
-
-interface MemoRe {
-  appName: string;
-  appVersion: string;
-  access: typeof access;
 }
 
 export const useMemoRe = (options?: Partial<MemoReOptions>) => {
@@ -17,6 +13,22 @@ export const useMemoRe = (options?: Partial<MemoReOptions>) => {
     access: access(baseUrl),
   };
 };
+
+//#region PlugIn
+interface MemoReAppOptions {
+  views: Record<string, Component>;
+}
+
+export const createMemoRe =
+  (options: MemoReAppOptions) =>
+  (app: App): any => {
+    const { views = {} } = options;
+
+    for (const key in views) {
+      app.component(key, views[key]);
+    }
+  };
+//#endregion
 
 //#region API
 interface AccessOptions {
