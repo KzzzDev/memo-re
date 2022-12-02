73c1be03e73b5da52f43c50478e151167ba688a9
KleinChiu <65886108+KleinChiu@users.noreply.github.com>
2022-12-02 15:17:30 +0000

    feat: :sparkles: Handle after login request

--- app/views/user/loginView.vue
+++ app/views/user/loginView.vue
@@ -2,20 +2,25 @@
   <form @submit.prevent="login">
     <input type="text" autocomplete="username" v-model="email" />
     <input type="password" autocomplete="password" v-model="password" />
+    <p v-if="!!error" class="error">Error in email and password</p>
     <input type="submit" value="ログイン" />
   </form>
 </template>
 
 <script setup lang="ts">
-import { ref } from "vue";
+import { ref, watch } from "vue";
+import { useRouter } from "vue-router";
 
 import { useBackend } from "../../lib/compositions";
 import { logIn } from "../../lib/network";
+
+const { push } = useRouter();
 
 const email = ref("");
 const password = ref("");
 
 const { data, error, refresh: login } = useBackend(logIn, false, { email, password });
+watch(data, (value) => !!value && push({ path: "/mypage" }));
 </script>
 
 <style scoped></style>
