058873f1a1279c4a148b21ab3c4ced1309769db3
KleinChiu <65886108+KleinChiu@users.noreply.github.com>
2022-12-02 10:48:58 +0000

    fix: :bug: Fix useBackend closure problem
    `useBackend` take a factory function as parameter.
    But the params of the call was not yet determine while in setup stage.
    Those values are usually of type `Ref<T>`.
    
    example:
    
    ```js
    const username = ref("")
    const password = ref("")
    useBackend(logIn({username, password}), false)
    ```
    
    `useBackend` is now modified to accept Ref type and non-Ref types.
    It will wrap parameters in to a `Reactive`
    and evaluate the internal value when it need to use.

--- app/lib/compositions.ts
+++ app/lib/compositions.ts
@@ -1,17 +1,26 @@
-import { shallowRef } from "vue";
+import { reactive, Ref, shallowRef } from "vue";
 
-import { BackendCall, APIResponse, ErrorResponse } from "./network";
+import { APIResponse, ErrorResponse } from "./network";
+
+type NestedRefs<T> = {
+  [key in keyof T]: T[key] extends object ? NestedRefs<T[key]> : Ref<T[key]>;
+};
+
+type BackendCall = (...params: any[]) => Promise<APIResponse<any>>;
+type BackendCallParameters<T extends BackendCall> = T extends (...args: infer P) => any ? NestedRefs<P> : never;
+type BackendCallResponse<T extends BackendCall> = ReturnType<T> extends Promise<infer I> ? I : APIResponse;
 
 /**
  * @example
  * const { data, error } = useBackend(getUserInfo("IdOfUser"))
  */
-export const useBackend = <T>(call: BackendCall<T>, immediately = false) => {
-  const data = shallowRef<APIResponse<T>>();
+export const useBackend = <T extends BackendCall>(call: T, immediately = true, ...params: BackendCallParameters<T>) => {
+  const data = shallowRef<BackendCallResponse<T>>();
   const error = shallowRef<ErrorResponse>();
+  const param = reactive(params);
 
   const refresh = () =>
-    call()
+    call(...param)
       .then((res) => (data.value = res))
       .catch((err) => (error.value = err));
 
