1b34dc549fe7dd92be9041a6a55d8e6898fa13f5
KleinChiu <65886108+KleinChiu@users.noreply.github.com>
2022-12-02 13:28:08 +0000

    feat: :poop: Login view

--- app/views/user/loginView.vue
+++ app/views/user/loginView.vue
@@ -1,11 +1,21 @@
 <template>
-
+  <form @submit.prevent="login">
+    <input type="text" autocomplete="username" v-model="email" />
+    <input type="password" autocomplete="password" v-model="password" />
+    <input type="submit" value="ログイン" />
+  </form>
 </template>
 
 <script setup lang="ts">
+import { ref } from "vue";
 
+import { useBackend } from "../../lib/compositions";
+import { logIn } from "../../lib/network";
+
+const email = ref("");
+const password = ref("");
+
+const { data, error, refresh: login } = useBackend(logIn, false, { email, password });
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
