5144d1ea0722ffb766f1b7d6f64dc5b9fc71841a
KleinChiu <65886108+KleinChiu@users.noreply.github.com>
2022-12-02 13:55:49 +0000

    docs: :bulb: Add docs for `useBackend`

--- app/lib/compositions.ts
+++ app/lib/compositions.ts
@@ -8,19 +8,32 @@ type NestedRefs<T> = {
 
 type BackendCall = (...params: any[]) => Promise<APIResponse<any>>;
 type BackendCallParameters<T extends BackendCall> = T extends (...args: infer P) => any ? NestedRefs<P> : never;
+/**
+ * Return type of backend call, basically same as `APIResponse<T>`.
+ */
 type BackendCallResponse<T extends BackendCall> = ReturnType<T> extends Promise<infer I> ? I : APIResponse;
 
 /**
+ * @param call network function.
+ * @param immediately whether to invoke the network function immediately on mounted.
+ * @param args parameters to call the function.
+ * @returns
  * @example
- * const { data, error } = useBackend(getUserInfo("IdOfUser"))
+ * const foo = ref("foo")
+ * const bar = ref("bar")
+ *
+ * const { data, error, refresh } = UseBackend(backendCall, true, foo, bar); // will call backendCall(foo, bar)
+ * // data will be filled if backend server responded normally.
+ * // error will be filled if backend server responded error.
+ * // refresh is a function to be call to fetch backend server again.
  */
-export const useBackend = <T extends BackendCall>(call: T, immediately = true, ...params: BackendCallParameters<T>) => {
+export const useBackend = <T extends BackendCall>(call: T, immediately = true, ...args: BackendCallParameters<T>) => {
   const data = shallowRef<BackendCallResponse<T>>();
   const error = shallowRef<ErrorResponse>();
-  const param = reactive(params);
+  const params = reactive(args);
 
   const refresh = () =>
-    call(...param)
+    call(...params)
       .then((res) => {
         data.value = res;
         error.value = undefined;
