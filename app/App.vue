633007070897ea95d1c8365fc2ebea65d6f48572
KleinChiu <65886108+KleinChiu@users.noreply.github.com>
2022-12-02 13:28:08 +0000

    chore: :fire: Remove unneeded code in App
    Remove all demo code and replace with `router-view`

--- app/App.vue
+++ app/App.vue
@@ -1,52 +1,9 @@
 <template>
-  <header>
-    <h1>{{ t("greeting") }}</h1>
-  </header>
-
-  <aside>
-    <select @change="change">
-      <option v-for="l in localeOptions" :key="l" :value="l">{{ l }}</option>
-    </select>
-  </aside>
-
   <main>
-    <m-click-me :start="0" />
+    <router-view />
   </main>
-
 </template>
 
-<script setup lang="ts">
-import { useI18n } from "vue-i18n";
+<script setup lang="ts"></script>
 
-import MClickMe from "./component/MClickMeButton.vue";
-
-import messages from "./public/locales/ja.json";
-
-const localeOptions = ["ja", "en"];
-
-const { t, setLocaleMessage, locale: current } = useI18n<{ message: typeof messages }, typeof localeOptions[number]>();
-
-const change = (ev: Event) => {
-  if (!(ev.target instanceof HTMLSelectElement)) {
-    return;
-  }
-
-  changeLocale(ev.target.value);
-};
-
-const changeLocale = (locale: typeof localeOptions[number]) =>
-  fetch(`locales/${locale}.json`)
-    .then((res) => res.json())
-    .then((message) => setLocaleMessage(locale, message))
-    .then(() => (current.value = locale))
-    .then(() => document.querySelector("html")?.setAttribute("lang", locale));
-
-
-
-</script>
-
-<style scoped>
-h1 {
-  color: green;
-}
-</style>
+<style scoped></style>
