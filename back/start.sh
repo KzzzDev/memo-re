01956ff4eaf4caeb371f796ad75bd543fd8952bc
Kazumasa1 <kazumasa4study@gmail.com>
2022-12-18 18:19:41 +0900

    add: 🌾Add comment

--- back/start.sh
+++ back/start.sh
@@ -5,6 +5,9 @@ python manage.py makemigrations
 python manage.py migrate
 # python manage.py collectstatic
 
+# django-debug-toolbar
+# staticファイルのロード先を別フォルダに設定しているため、CSSが効かない問題を解決
+# https://unisia-se.com/detail/33/
 cp -r /usr/local/lib/python3.9/site-packages/debug_toolbar/static/debug_toolbar /usr/src/app/app/static
 
 # configアプリuWSGIに接続。「--py-autoreload 1」でファイル等に変更があった際は自動リロード
