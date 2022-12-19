d3356783c2c179eb5e91ed5c9de459b55a39c884
Yuuma-Ujimoto <yxy.yuuma@gmail.com>
2022-12-20 00:00:31 +0900

    feat:Vue用のFontAwesomeライブラリ追加・初期設定

--- web/app/main.ts
+++ web/app/main.ts
@@ -4,4 +4,16 @@ import App from "./App.vue";
 
 import { i18n, router, memoRe } from "./plugin";
 
-createApp(App).use(i18n).use(router).use(memoRe).mount("#app");
+import {library} from "@fortawesome/fontawesome-svg-core";
+import {FontAwesomeIcon} from "@fortawesome/vue-fontawesome"
+import {faUserSecret} from "@fortawesome/free-solid-svg-icons";
+
+library.add(faUserSecret)
+
+
+createApp(App)
+    .use(i18n)
+    .use(router)
+    .use(memoRe)
+    .component("font-awesome-icon",FontAwesomeIcon)
+    .mount("#app");
