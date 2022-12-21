4892598d07c27850c3df821b25b812032aadef30
Yuuma-Ujimoto <yxy.yuuma@gmail.com>
2022-12-21 16:44:45 +0900

    feat:TailwindCSS導入

--- web/app/main.ts
+++ web/app/main.ts
@@ -3,13 +3,21 @@ import {createApp} from "vue";
 import App from "./App.vue";
 
 import {i18n, router, memoRe} from "./plugin";
+import "./index.css"
 
 import {library} from "@fortawesome/fontawesome-svg-core";
 import {FontAwesomeIcon} from "@fortawesome/vue-fontawesome"
 import {faUser, faPenToSquare} from "@fortawesome/free-regular-svg-icons";
-import {faArrowUpFromBracket, faUserGroup, faMagnifyingGlass,faCircleInfo,faBell,faRightFromBracket} from "@fortawesome/free-solid-svg-icons";
+import {
+    faArrowUpFromBracket,
+    faUserGroup,
+    faMagnifyingGlass,
+    faCircleInfo,
+    faBell,
+    faRightFromBracket
+} from "@fortawesome/free-solid-svg-icons";
 
-library.add(faUser, faPenToSquare, faArrowUpFromBracket, faUserGroup, faMagnifyingGlass,faCircleInfo,faBell,faRightFromBracket)
+library.add(faUser, faPenToSquare, faArrowUpFromBracket, faUserGroup, faMagnifyingGlass, faCircleInfo, faBell, faRightFromBracket)
 
 
 createApp(App)
