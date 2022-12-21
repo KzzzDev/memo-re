cc02b9e96665886b9d6791e5e289b5a4523f7afe
KleinChiu <65886108+KleinChiu@users.noreply.github.com>
2022-12-21 15:44:22 +0000

    feat: :sparkles: Add editing switch

--- web/app/views/user/mypageView.vue
+++ web/app/views/user/mypageView.vue
@@ -1,11 +1,17 @@
 <template>
+  <h1>
+    <img src="/images/logo.png" alt="" width="96" />
+    My Page
+  </h1>
 
+  <template v-if="editing"> editing </template>
+  <template v-else> normal </template>
 </template>
 
 <script setup lang="ts">
+import { ref } from "vue";
 
+const editing = ref(false);
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
