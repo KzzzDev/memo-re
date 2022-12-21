44390c3b3f69e7fb33f3488095efc77f372d6034
KleinChiu <65886108+KleinChiu@users.noreply.github.com>
2022-12-21 15:41:09 +0000

    feat: :sparkles: Provide user info across app
    There are components and views referencing to the current user's info.
    App root provide reactive user info with `UserInfoKey`.
    Using `UserInfoKey` to inject into child components and views.

--- web/app/App.vue
+++ web/app/App.vue
@@ -1,18 +1,40 @@
 <template>
-  <header>
-
-  </header>
+  <header></header>
 
   <main>
-    <router-view/>
+    <router-view @update:user-info="refreshUser" />
   </main>
 </template>
 
-<script setup lang="ts">
+<script lang="ts">
+import { Ref } from "vue";
 
+import { User, UserCredential } from "./lib/network";
+
+/**
+ * Key to inject user info into components.
+ */
+export const UserInfoKey = Symbol();
+type _UserInfo = User & Pick<UserCredential, "email">;
+/**
+ * Type of user info to be injected.
+ *
+ * @see {@link UserInfoKey}
+ */
+export type UserInfo = Ref<_UserInfo>;
 </script>
 
-<style>
+<script setup lang="ts">
+import { provide, ref, watch } from "vue";
+import { useBackend } from "./lib";
+import { getUserInfo } from "./lib/network";
 
+// #region Provide user info across the app
+const { data: userRes, refresh: refreshUser } = useBackend(getUserInfo);
+const userInfo = ref<_UserInfo>();
+watch(userRes, (value) => (userInfo.value = value?.user));
+provide(UserInfoKey, userInfo);
+// #endregion
+</script>
 
-</style>
+<style></style>
