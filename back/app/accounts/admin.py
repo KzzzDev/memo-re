b096b245d42f805fd0c369d30794c21a05dd2e7b
nagi <ymxlinkinparkxlc@gmail.com>
2022-12-24 21:12:06 +0900

    add: memore.models

--- back/app/accounts/admin.py
+++ back/app/accounts/admin.py
@@ -5,13 +5,17 @@ from .models import CustomUser
 class CustomerAdmin(admin.ModelAdmin):
     list_display = (
         'userid', 'username', 'email', 'icon', 'tag', 'date_joined', 'last_login',
-        'is_superuser', 'is_staff', 'is_active')
-    list_display_links = ('userid', 'username', 'email',)
+        'is_superuser', 'is_staff', 'is_active'
+    )
+    list_display_links = 'userid'
     search_fields = (
         'userid', 'username', 'email', 'icon', 'tag', 'date_joined', 'last_login',
-        'is_superuser', 'is_staff', 'is_active')
+        'is_superuser', 'is_staff', 'is_active'
+    )
     list_filter = (
-        'date_joined', 'last_login', 'is_superuser', 'is_staff', 'is_active')
+        'userid', 'username', 'email', 'icon', 'tag', 'date_joined', 'last_login',
+        'is_superuser', 'is_staff', 'is_active'
+    )
 
 
 admin.site.register(CustomUser, CustomerAdmin)
--- back/app/memore/models.py
+++ back/app/memore/models.py
@@ -38,7 +38,7 @@ class Friend(models.Model):
     )
 
     def __str__(self):
-        return str(self.left)
+        return str(self.left) + "+" + str(self.right)
 
     # 未入力の際のエラーとleftとrightが同じ際のエラーをまとめたもの
     def clean(self):
@@ -51,25 +51,82 @@ class Friend(models.Model):
             pass
 
 
-# class Note(models.Model):
-#     """ノートモデル"""
-#
-#     id =
-#
-#     class Meta(object):
-#         db_table = 'note'
-#         verbose_name = verbose_name_plural = 'ノート'
-#
-#     title =
-#     keyword =
-#
-#
-#     def __str__(self):
-#         return str(self.id)
-#
-#
-# class NoteShare(models.Model):
-#
-#
-#     def __str__(self):
-#         return str(self.)
+class Note(models.Model):
+    """ノートモデル"""
+
+    class Meta(object):
+        db_table = 'note'
+        verbose_name = verbose_name_plural = 'ノート'
+
+    title = models.CharField(
+        _("タイトル"),
+        max_length=30,
+        blank=True, null=True,
+        # validators=[title_validator],
+    )
+    creator = models.ForeignKey(
+        CustomUser,
+        verbose_name="作成者",
+        related_name="creator",
+        on_delete=models.CASCADE,
+    )
+    keyword = models.CharField(
+        _("キーワード"),
+        max_length=30,
+        blank=True, null=True,
+        # validators=[keyword_validator],
+    )
+    text = models.ImageField(_('テキスト'), upload_to='text', blank=True, null=True)
+    image = models.ImageField(_('画像'), upload_to='image', blank=True, null=True)
+    category = models.IntegerField(
+        _("カテゴリ"),
+        blank=True, null=True
+    )
+    is_active = models.BooleanField(
+        _("active"),
+        default=True,
+        help_text=_(
+            "Designates whether this user should be treated as active. "
+            "Unselect this instead of deleting accounts."
+        ),
+    )
+
+    def __str__(self):
+        return str(self.id)
+
+
+class NoteShare(models.Model):
+    """ノート共有モデル"""
+
+    class Meta(object):
+        db_table = 'note_share'
+        verbose_name = verbose_name_plural = 'ノート共有'
+
+        constraints = [
+            # user_idとnote_idでユニーク制約
+            models.UniqueConstraint(fields=['user_id', 'note_id'], name='unique_note_share')
+        ]
+
+    user_id = models.ForeignKey(
+        CustomUser,
+        verbose_name="ユーザーID",
+        related_name="user_id",
+        on_delete=models.CASCADE,
+    )
+    note_id = models.ForeignKey(
+        Note,
+        verbose_name="ノートID",
+        related_name="note_id",
+        on_delete=models.CASCADE,
+    )
+    is_active = models.BooleanField(
+        _("active"),
+        default=True,
+        help_text=_(
+            "Designates whether this user should be treated as active. "
+            "Unselect this instead of deleting accounts."
+        ),
+    )
+
+    def __str__(self):
+        return str(self.user_id) + "+" + str(self.note_id)
