92ed0321a37781ac84b912eb6bf5fd4e79ce4c96
nagi <ymxlinkinparkxlc@gmail.com>
2023-01-06 03:20:59 +0900

    update: modelを最新のものに更新

--- back/app/memore/admin.py
+++ back/app/memore/admin.py
@@ -3,30 +3,24 @@ from .models import Friend, Note, NoteShare
 
 
 class FriendAdmin(admin.ModelAdmin):
-    list_display = ('userid_id', 'friend', 'approval',
-                    'register_at', 'is_deleted')
-    list_display_links = ('userid_id', 'friend',)
-    search_fields = ('userid_id__userid', 'friend__userid',
-                     'approval', 'register_at', 'is_deleted')
-    list_filter = ('userid_id', 'friend', 'approval',
-                   'register_at', 'is_deleted')
+    list_display = ('id', 'own_id', 'friend_id', 'approval', 'register_at')
+    list_display_links = ('id',)
+    search_fields = ('own_id__id', 'friend_id__id', 'approval', 'register_at')
+    list_filter = ('own_id', 'friend_id', 'approval', 'register_at')
 
 
 class NoteAdmin(admin.ModelAdmin):
-    list_display = ('id', 'title', 'creator', 'keyword',
-                    'text', 'image', 'category', 'is_active')
+    list_display = ('id', 'user_id', 'title', 'keyword', 'text_uri', 'image_uri', 'created_at', 'is_active', 'is_public')
     list_display_links = ('id',)
-    search_fields = ('title', 'creator__userid', 'keyword',
-                     'text', 'image', 'category', 'is_active')
-    list_filter = ('title', 'creator', 'keyword', 'text',
-                   'image', 'category', 'is_active')
+    search_fields = ('user_id__id', 'title', 'keyword', 'text_uri', 'image_uri', 'created_at', 'is_active', 'is_public')
+    list_filter = ('user_id', 'title', 'keyword', 'text_uri', 'image_uri', 'created_at', 'is_active', 'is_public')
 
 
 class NoteShareAdmin(admin.ModelAdmin):
-    list_display = ('user_id', 'note_id', 'is_active')
-    list_display_links = ('user_id', 'note_id',)
-    search_fields = ('user_id__userid', 'note_id__id', 'is_active')
-    list_filter = ('user_id', 'note_id', 'is_active')
+    list_display = ('id', 'own_id', 'friend_id', 'note_id', 'notified', 'apply')
+    list_display_links = ('id',)
+    search_fields = ('own_id__id', 'friend_id__id', 'note_id__id', 'notified', 'apply')
+    list_filter = ('own_id', 'friend_id', 'note_id', 'notified', 'apply')
 
 
 admin.site.register(Friend, FriendAdmin)
