7ba206378023dc38368aa927c9b34eac96709158
Kazumasa1 <kazumasa4study@gmail.com>
2023-01-10 01:50:40 +0900

    add: ノート共有モデル用のシリアライザを追加

--- back/app/apiv1/serializers.py
+++ back/app/apiv1/serializers.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
 from memore.models import Friend
 from memore.models import Note
+from memore.models import NoteShare
 
 
 class FriendSerializer(serializers.ModelSerializer):
@@ -16,4 +17,14 @@ class NoteSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Note
+        fields = ('id', 'user', 'title', 'keyword', 'text_uri',
+                  'image_uri', 'created_at', 'is_public')
+        read_only_fields = ('id', 'created_at')
+
+
+class NoteShareSerializer(serializers.ModelSerializer):
+    """ノート共有モデル用のシリアライザ"""
+
+    class Meta:
+        model = NoteShare
         fields = '__all__'
