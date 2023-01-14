f84e908a24a9bc81b94d63852d90fa038f236b27
Kazumasa1 <kazumasa4study@gmail.com>
2023-01-15 04:10:15 +0900

    change: 送信されたパスワードをハッシュ化

--- back/app/accounts/serializers.py
+++ back/app/accounts/serializers.py
@@ -1,9 +1,14 @@
 from djoser import serializers
 from accounts.models import CustomUser
+from django.contrib.auth.hashers import make_password
 
 
 # djoserのシリアライザーを変更する場合
 class CustomUserSerializer(serializers.UserSerializer):
+
+    def validate_password(self, value: str) -> str:
+        return make_password(value)
+
     class Meta:
         model = CustomUser
         fields = ['id', 'username', 'icon_uri', 'email', 'password', 'tag']
