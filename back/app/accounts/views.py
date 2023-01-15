ffc83118d85287a370e2a6d18b572321aadd9553
Kazumasa1 <kazumasa4study@gmail.com>
2023-01-15 18:55:29 +0900

    change: アカウント情報変更にパスワードの整合性チェックを追加

--- back/app/accounts/views.py
+++ back/app/accounts/views.py
@@ -1,3 +1,31 @@
 from django.shortcuts import render
+from djoser.views import UserViewSet
+from rest_framework.decorators import action
+from accounts.models import CustomUser
+from rest_framework.response import Response
 
-# Create your views here.
+
+class CustomUserViewSet(UserViewSet):
+
+    @action(["get", "patch", "delete"], detail=False)
+    # @action(["get", "put", "patch", "delete"], detail=False)
+    def me(self, request, *args, **kwargs):
+
+        self.get_object = self.get_instance
+        if request.method == "GET":
+            return self.retrieve(request, *args, **kwargs)
+        # elif request.method == "PUT":
+        #     return self.update(request, *args, **kwargs)
+        elif request.method == "PATCH":
+            # if 'password' in request.data:
+            #     queryset = CustomUser.objects.filter(
+            #         password=request.data['password'])
+            # else:
+            #     return Response(data={'error': 'passwordが未入力です'}, status=500)
+            # if queryset.exists():
+            #     return self.partial_update(request, *args, **kwargs)
+            # else:
+            #     return Response(data={'error': 'passwordが違います'}, status=500)
+            return self.partial_update(request, *args, **kwargs)
+        elif request.method == "DELETE":
+            return self.destroy(request, *args, **kwargs)
