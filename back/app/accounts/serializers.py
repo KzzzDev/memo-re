71e74272bf7888d9d87ae276194e77f0f9d64951
Kazumasa1 <kazumasa4study@gmail.com>
2023-01-18 04:33:34 +0900

    change: ユーザ検索APIのレスポンスボディを修正

--- back/app/accounts/serializers.py
+++ back/app/accounts/serializers.py
@@ -23,3 +23,19 @@ class CustomUserSerializer(serializers.UserSerializer):
         request = self.context.get('request')
         photo_url = obj.fingerprint.url
         return request.build_absolute_uri(photo_url)
+
+
+class CustomUserSearchSerializer(serializers.UserSerializer):
+
+    def validate_password(self, value: str) -> str:
+        return make_password(value)
+
+    class Meta:
+        model = CustomUser
+        fields = ['id', 'username', 'icon_uri', 'tag']
+        read_only_fields = ('id', 'username', 'icon_uri', 'tag')
+
+    def get_icon_uri(self, obj):
+        request = self.context.get('request')
+        photo_url = obj.fingerprint.url
+        return request.build_absolute_uri(photo_url)
