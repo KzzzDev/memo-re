d89c0ea83a4f9be889a23955ea59595286ca2094
reone19 <reone1999@gmail.com>
2023-01-18 02:00:12 +0900

    remove: aiディレクトリの不要なデータを削除

--- ai/fast-api/module/scripts/sender.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-scp -i /home/th458-user/.ssh/HAL-TH22-GP41-TH458-VM-web01_key.pem th458-user@20.66.79.230:/home/fast-api/module/test_scp.txt th458-user@20.243.210.134:/home/th458-user/
-
