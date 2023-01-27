7b7b54edc5eaa70e7e0ea227d44760fcf977e7a1
Kazumasa <70145199+Kazumasa1@users.noreply.github.com>
2023-01-27 22:59:08 +0900

    img_uriとerrorの修正
    img_uri → image_uri
    error → errorFlag

--- web/app/src/views/FriendImageView.vue
+++ web/app/src/views/FriendImageView.vue
@@ -28,7 +28,7 @@
             v-for="img in scrollData"
             v-bind:key="img"
             class="scrImg"
-            @click="ImageView(img.id, img.title, img.keyword,img.img_uri, img.text_uri, img.is_public, img.created_at)"
+            @click="ImageView(img.id, img.title, img.keyword,img.image_uri, img.text_uri, img.is_public, img.created_at)"
           >
             <img :src="ImgSrc(img.image_uri)" alt="画像" />
             <p class="opacity">{{ img.title }}</p>
@@ -40,7 +40,7 @@
       <div class="modalContent">
         <h2>共有申請しますか？</h2>
         <img :src="image_uri" alt="画像" />
-        <p v-if="error != '' ">{{ error }}</p>
+        <p v-if="errorFlag == true">すでに相手から申請が来ています</p>
         <div class="flex button">
           <button class="cancel" @click="Cancel()">キャンセル</button>
           <button class="accept" @click="Share()">申請</button>
@@ -69,7 +69,7 @@ export default {
       keywordAry: "",
       is_public: null,
       modalFlag: false,
-      error: "",
+      errorFlag: false,
       time: "",
     };
   },
@@ -106,7 +106,7 @@ export default {
       localStorage.setItem("noteId", noteId);
       this.title = title;
       this.keyword = keyword.split(",");
-      this.img_uri = IMG_URL + img_uri;
+      this.image_uri = IMG_URL + img_uri;
       this.text_uri = text_uri;
       this.is_public = is_public;
       this.time = time.split("T");
@@ -143,7 +143,8 @@ export default {
     Share() {
       const requestBody = {
         user_from: parseInt(this.user),
-        note: parseInt(this.id)
+        note: parseInt(this.id),
+        get: true,
       };
       console.log(requestBody);
       const token = this.$cookies.get("access");
@@ -155,8 +156,8 @@ export default {
           this.modalFlag = !this.modalFlag;
           return;
         })
-        .catch((response) => {
-          this.error = response.response.data.error;
+        .catch(() => {
+          this.errorFlag = true;
         });
     },
   },
