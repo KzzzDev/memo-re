4fc52919a7060d4e572997d6437ce7cdf8e5c7b9
Kazumasa1 <kazumasa4study@gmail.com>
2023-01-28 00:09:27 +0900

    clean: 不要なコードを削除

--- web/app/src/views/ShareDrop.vue
+++ web/app/src/views/ShareDrop.vue
@@ -32,7 +32,7 @@
           <p>{{ modalUser }}</p>
           <p v-if="errorFlag == true">すでに相手から申請が来ています</p>
           <div class="flex button">
-            <button class="cancel" @click="Cancel()">キャンセル</button>￥
+            <button class="cancel" @click="Cancel()">キャンセル</button>
             <button class="accept" @click="Share()">共有</button>
           </div>
         </div>
